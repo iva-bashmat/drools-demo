80cbe65d7a6d6bde554fe4d2a11a42da3fcf0a5e
Iva Horoshko <iva.horoshko@gmail.com>
2022-06-26 22:17:10 +0200

    remove unused code

--- src/test/kotlin/com/example/drools/service/RulesServiceTest.kt
+++ src/test/kotlin/com/example/drools/service/RulesServiceTest.kt
@@ -1,7 +1,6 @@
 package com.example.drools.service
 
 import com.example.drools.model.Route
-import com.example.drools.model.RouteResult
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,33 +13,20 @@ class RulesServiceTest {
     private lateinit var rulesService: RulesService
 
     @Test
-    fun `testAccept`() {
-        var routeAccept = Route(47250, 3)
-
-        var routeResult = RouteResult(1)
-        routeResult.setState("no")
-
+    fun testAccept() {
+        val routeAccept = Route(47250, 3)
         assertEquals("accepted", rulesService.applyRule(routeAccept).getState())
     }
 
     @Test
-    fun `testReject`() {
-        var routeReject = Route(47250, 1027)
-        var routeIgnore = Route(47250, 231)
-
-        var routeResult = RouteResult(1)
-        routeResult.setState("no")
-
+    fun testReject() {
+        val routeReject = Route(47250, 1027)
         assertEquals("rejected", rulesService.applyRule(routeReject).getState())
     }
 
     @Test
-    fun `testUnknown`() {
-        var routeIgnore = Route(47250, 231)
-
-        var routeResult = RouteResult(1)
-        routeResult.setState("no")
-
+    fun testUnknown() {
+        val routeIgnore = Route(47250, 231)
         assertEquals("unknown", rulesService.applyRule(routeIgnore).getState())
     }
 }
